fef8f5792527a3cd1fa9c2bde9c371f09a5c198d
PatH5 <43816130+harshpatel5@users.noreply.github.com>
2025-11-09 23:30:55 -0500

    working code of cash register

--- db/models.py
+++ db/models.py
@@ -14,3 +14,12 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+class products(models.Model):
+    upc = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    
+
+    def __str__(self):
+        return f"{self.upc} - {self.name} (${self.price})"
